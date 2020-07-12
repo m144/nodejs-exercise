ca121bd5bbb148475fb05968c8ee8f9bf3c8cf14
Manuel <manulei@gmail.com>
2020-07-12 08:29:02 +0000

    default route for 404

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,10 +1,14 @@
 'use strict';
-module.exports = function(app) {
-  const users = require('../controllers/UsersController');
-  const { userValidationRules, validate } = require('../../validator.js');
+module.exports = app => {
+	const users = require('../controllers/UsersController');
+	const { userValidationRules, validate } = require('../../validator.js');
 
-  // user Routes
-  app.get('/users', users.all);
-  app.post('/users', userValidationRules(), validate, users.create);
-  app.delete('/users/:userId', users.delete);
+	// user Routes
+	app.get('/users', users.all);
+	app.post('/users', userValidationRules(), validate, users.create);
+	app.delete('/users/:userId', users.delete);
+
+	app.use((req, res) => {
+		res.status(404).send({url: req.originalUrl + ' not found'});
+	});
 };
